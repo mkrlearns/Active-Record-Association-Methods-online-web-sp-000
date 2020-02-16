dd217b2f4e0da8ddc224deca9356a71666d6aaf4
ryan m <contact@ryanmeek.com>
2020-02-16 21:25:47 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,10 +2,10 @@ class Genre < ActiveRecord::Base
   has_many :songs
   has_many :artists, through: :songs
 
-  def song_count() songs.count end
+  def song_count; songs.count end
 
-  def artist_count() artists.count end
+  def artist_count; artists.count end
 
-  def all_artist_names() artists.collect { |artist| artist.name } end
+  def all_artist_names; artists.collect { |artist| artist.name } end
 
 end
