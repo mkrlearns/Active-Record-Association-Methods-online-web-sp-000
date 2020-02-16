3d5c5b357c6929c84a9baa3f3792a607c04e5df5
ryan m <contact@ryanmeek.com>
2020-02-16 21:25:26 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
 
-  def get_genre_name() genre.name end
+  def get_genre_name; genre.name end
 
   def drake_made_this; self.artist = Artist.find_or_create_by(name: "Drake") end
 
