2aa88576b2929a89ad364ecc8a436c28f05169bd
ryan m <contact@ryanmeek.com>
2020-02-16 21:12:48 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,6 +4,6 @@ class Song < ActiveRecord::Base
 
   def get_genre_name() genre.name end
 
-  def drake_made_this() self.artist = Artist.new(name: "Drake") end
+  def drake_made_this() self.artist = Artist.create(name: "Drake") end
 
 end
