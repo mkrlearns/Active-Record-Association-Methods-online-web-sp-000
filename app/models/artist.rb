64bcc588dd6028e1898c46682a71c2ca2e011ce6
ryan m <contact@ryanmeek.com>
2020-02-16 21:26:00 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,12 +2,12 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
-  def get_first_song() songs.first end
+  def get_first_song; songs.first end
 
-  def get_genre_of_first_song() songs.first.genre end
+  def get_genre_of_first_song; songs.first.genre end
 
-  def song_count() songs.count end
+  def song_count; songs.count end
 
-  def genre_count() genres.count end
+  def genre_count; genres.count end
 
 end
